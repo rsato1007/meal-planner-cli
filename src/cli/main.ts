0d01fbc1907bfa16a0c22bc320683c99c3ce8477
rsato1007 <robertmsato@gmail.com>
2024-09-04 18:35:05 -0600

    removing some comments no longer necessary

--- src/cli/main.ts
+++ src/cli/main.ts
@@ -38,9 +38,6 @@ const program = new Command();
     .description('CLI tool to allow users to plan meals.')
     .version('0.9.5')
 
-    /**
-     * Look towards refactoring types on this command. It might take some extensive refactoring and thinking as a heads up.
-     */
     program.command('add')
         .description('Add a dish to your meal planner')
         .argument('<string>', 'name of dish to add')
@@ -55,14 +52,6 @@ const program = new Command();
             }
         });
 
-    /**
-     * I would approach this by allowing users to specify meal type, entree type, day etc. and by allowing these to be blank.
-     * Allow the system to find the removed meal if needed. For now, we'll start by requiring everything.
-     * 
-     * @remarks
-     * - There is a small issue where without the finally statement, the process doesn't conclude.
-     *   Until I can determine what's causing it, this will be left as is.
-     */
     program.command('remove')
     .description('Remove a dish from your meal planner')
     .argument('<string>', 'name of dish to remove')
@@ -92,12 +81,6 @@ const program = new Command();
         }
     });
 
-    /*
-        Some things to consider: 
-            (1) How simple and restrictive we can be starting off 
-            (2) How to display the information in a good visual style.
-            (3) How to avoid showing days, times, and meal types with nothing in there.
-    */
     program.command('show')
     .description('Displays recipes based on specified description.')
     .option(defaults[0][0], defaults[0][1])
